d16095b6b58338546cdfe1f6dee2f940ef3c110d
Kaspars Purens <kaspars@purens.lv>
2024-06-21 17:06:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <!-- <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <main>
+    <RouterView />
+  </main>
+</template>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex">great</div>
+</template>
