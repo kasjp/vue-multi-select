c557b902ca81925f2011f3a0c788607f3b98840e
Kaspars Purens <kaspars@purens.lv>
2024-06-26 14:27:11 +0300

    feat: add dev page for testing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import KMultiSelect from "@/components/KMultiSelect.vue";
+import { reactive } from "vue";
+
+const currencies = reactive({
+  EUR: false,
+  PLN: false,
+  GEL: false,
+  DKK: false,
+  CZK: false,
+  GBP: false,
+  SEK: false,
+  USD: false,
+  RUB: false
+});
 </script>
 
 <template>
-  <!-- <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-  <main>
-    <RouterView />
-  </main>
+  <div class="flex justify-center items-center h-full">
+    <k-multi-select
+      class="w-2xl"
+      :model-value="currencies"
+      @update:model-value="console.log"
+    />
+  </div>
 </template>
+<style>
+html,
+body,
+body > div#app,
+main {
+  width: 100%;
+  height: 100%;
+}
+html {
+  font-family: "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
