fda60532ec1bd6df238f6cc8fec0ec1cc0770f55
Kaspars Purens <kaspars@purens.lv>
2024-06-26 14:28:00 +0300

    feat: exporting library components

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "virtual:uno.css";
+import KMultiSelect from "./components/KMultiSelect.vue";
+import KCheckbox from "@/core/components/KCheckbox.vue";
+import KChip from "@/core/components/KChip.vue";
+
+export { KMultiSelect, KCheckbox, KChip };
